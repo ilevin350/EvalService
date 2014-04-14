b16abde895c5a51b9a1e2607053e98bf6d020acf
Ira Levin <ilevin@bdata.com>
2014-04-14 16:18:17 -0700

    lesson 2

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.EvalServiceReference;
+
+namespace Client
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Hit ENTER to start the application... ");
+      Console.ReadLine();
+
+      EvalServiceClient client = new EvalServiceClient("wsHttp");
+
+      Eval eval = new Eval {Comments = "This came from code", Submitter = "Ira", TimeSubmitted = DateTime.Now};
+      
+      client.SubmitEval(eval);
+
+      Eval[] evals = client.GetEvals();
+
+      foreach (Eval ev in evals)
+      {
+        Console.WriteLine(String.Format("Comment: {0}", ev.Comments));
+      }
+
+      Console.Write("Hit ENTER to exit the application... ");
+      Console.ReadLine();
+    }
+  }
+}
