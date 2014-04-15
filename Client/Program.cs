331fb904cf329366d85fdeb81db4aedb801388cd
Ira Levin <ilevin@bdata.com>
2014-04-14 19:12:08 -0700

    Refresh service reference

--- Client/Program.cs
+++ Client/Program.cs
@@ -15,7 +15,8 @@ namespace Client
       Console.Write("Hit ENTER to start the application... ");
       Console.ReadLine();
 
-      EvalServiceClient client = new EvalServiceClient("wsHttp");
+      // Pass in the name of the endpoint you want to connect to on the service
+      EvalServiceClient client = new EvalServiceClient("ws");
 
       Eval eval = new Eval {Comments = "This came from code", Submitter = "Ira", TimeSubmitted = DateTime.Now};
       
